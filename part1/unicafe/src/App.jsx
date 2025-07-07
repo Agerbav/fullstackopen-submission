d49183eff89612408d295b071db7a9e4704e8a07
Agerbav <vabregass118@gmail.com>
2025-07-07 19:28:10 +0700

    1.7: unicafe step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 
+
 const Display = ({text, total}) => {
   return (
     <div>
@@ -21,15 +22,29 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+
 
   const increaseGood = () => {
+    const prevGood = good + 1;
+    setTotal(prevGood + bad + neutral)
     setGood(good + 1)
-  }
+  } 
   const increaseNeutral = () => {
+    const prevNeutral = neutral + 1;
+    setTotal(good + bad + prevNeutral)
     setNeutral(neutral + 1)
   }
   const increaseBad = () => {
+    const prevBad = bad + 1;
+    setTotal(good + prevBad + neutral)
     setBad(bad + 1)
+  }
+  const calculateAverage = () => {
+    return (good - bad) / total
+  }
+  const calculatePositive = () => {
+    return (good / total)*100 + "%"
   }
 
   return (
@@ -46,6 +61,9 @@ const App = () => {
       <Display text="good" total={good}/>
       <Display text="neutral" total={neutral}/>
       <Display text="bad" total={bad}/>
+      <Display text="all" total={total}/>
+      <Display text="average" total={calculateAverage()}/>
+      <Display text="positive" total={calculatePositive()}/>
     </div>
   )
 }
