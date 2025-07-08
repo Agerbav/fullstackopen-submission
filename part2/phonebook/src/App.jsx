3bf2fb1a0cf7f538e5c5617a24f36ff111744f1d
Agerbav <vabregass118@gmail.com>
2025-07-08 16:53:02 +0700

    2.10: The Phonebook Step 5

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,33 @@
 import { useState } from 'react'
 
+const Filter = ({filter, handleFilterChange}) =>{
+  return(
+    <div>
+      Filter shown with <input value={filter} onChange={handleFilterChange}/>
+    </div>
+  )
+}
+const PersonForm = ({newName, handleNameChange, newNumber, handleNumberChange, addPerson}) =>{
+  return (
+    <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+  )
+}
+const Person = ({person}) => {
+  return (
+    <li>{person.name} {person.number}</li>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456', id: 1 },
@@ -41,23 +69,13 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>Filter shown with <input value={filter} onChange={handleFilterChange}/></div>
-      <h2>Add a new</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange}/>
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <Filter filter={filter} handleFilterChange={handleFilterChange}/>
+      <h3>Add a new</h3>
+      <PersonForm newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange} addPerson={addPerson}/>
       <h2>Numbers</h2>
       <ul>
         {personsToShow.map(person => 
-          <li key={person.name}>{person.name} {person.number}</li>
+          <Person key={person.name} person={person} />
         )}
       </ul>
     </div>
