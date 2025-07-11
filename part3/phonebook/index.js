13cafc6124531cf4964a4c9be086297c24d58dca
Agerbav <vabregass118@gmail.com>
2025-07-11 14:23:33 +0700

    3.8*: Phonebook backend step 8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,7 +2,12 @@ const express = require("express")
 const app = express()
 const morgan  = require("morgan")
 app.use(express.json())
-app.use(morgan("tiny"))
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :content"))
+
+morgan.token('content', (req, res) => {
+  console.log(req.body)
+  return JSON.stringify(req.body)
+})
 
 let persons = [
     { 
