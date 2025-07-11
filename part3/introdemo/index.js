5303308d1a58349a1d023a78cb44ba8d5df023f2
Agerbav <vabregass118@gmail.com>
2025-07-11 19:37:43 +0700

    Updated introdemo

--- part3/introdemo/index.js
+++ part3/introdemo/index.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 const express = require('express')
-// const cors = require('cors')
+const Note = require('./models/note')
+
 const app = express()
 
 app.use(express.json())
-// app.use(cors())
 app.use(express.static('dist'))
 
 //Middleware used when send a request to the server
@@ -23,41 +24,21 @@ app.use(requestLogger)
 
 // app.use(unknownEndpoint)
 
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
-
 app.get('/', (request, response) => {
   response.send('<h1>Hello!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
   const id = request.params.id
-  const note = notes.find(note=>note.id===id)
-
-  if(note){
+  Note.findById(id).then(note=>{
     response.json(note)
-  }else{
-    response.status(404).end()
-  }
+  })
 
 })
 
@@ -83,15 +64,14 @@ app.post('/api/notes', (request, response) => {
     })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then(savedNote=>{
+    response.json(savedNote)
+  })
 })
 
 const PORT = process.env.PORT || 3001
