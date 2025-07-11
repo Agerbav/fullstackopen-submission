2c737b0a9cf047c34a7ac149b4033023ff4c3788
Agerbav <vabregass118@gmail.com>
2025-07-11 19:38:06 +0700

    3.13: Phonebook database, step 1

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,9 @@
+require('dotenv').config()
 const express = require("express")
-const app = express()
 const morgan  = require("morgan")
+const Person = require('./models/person')
+const app = express()
+
 app.use(express.json())
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :content"))
 app.use(express.static('dist'))
@@ -10,32 +13,13 @@ morgan.token('content', (req, res) => {
   return JSON.stringify(req.body)
 })
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
 //get all persons
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(people=>{
+    response.json(people)
+  })
+  
 })
 
 //get info for current state
