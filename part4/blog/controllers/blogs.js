16eb9a10e15e0e7cdd8ac531c6d4dc3bf95d377b
Agerbav <vabregass118@gmail.com>
2025-07-15 16:45:29 +0700

    Reapply "4.22*: Blog List Expansion, step 10"
    This reverts commit 3f381e9dd1424cad88c9af63fb4ebe425db11576.

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -2,21 +2,16 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
-const tokenExtractor = require('../utils/middleware').tokenExtractor
+const { userExtractor } = require('../utils/middleware')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', {username: 1, name:1, id: 1})
   response.json(blogs)
 })
 
-blogsRouter.post('/', tokenExtractor, async (request, response) => {
+blogsRouter.post('/', userExtractor, async (request, response) => {
   const body = request.body
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-
-  if(!decodedToken.id){
-    return response.status(401).json({ error: 'token invalid' })
-  }
-  const user = await User.findById(decodedToken.id)
+  const user = request.user
 
   if(!body.title || !body.url){
     return response.status(400).end()
@@ -36,13 +31,9 @@ blogsRouter.post('/', tokenExtractor, async (request, response) => {
   response.status(201).json(savedBlog)
 })
 
-blogsRouter.delete('/:id', async (request, response) => {
+blogsRouter.delete('/:id', userExtractor, async (request, response) => {
   const blog = await Blog.findById(request.params.id)
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  if(!decodedToken.id){
-    return response.status(401).json({ error: 'token invalid' })
-  }
-  const user = await User.findById(decodedToken.id)
+  const user = request.user
   if(user.id.toString() === blog.user.toString()){
     await Blog.findByIdAndDelete(request.params.id)
     response.status(204).end()
